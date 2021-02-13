8724b48ecd9b31afe78c58fbf02b967162b739d3
Igor Pereira <igorpereira120@gmail.com>
2021-02-13 04:12:49 -0300

    OrderStatus enum

--- src/main/java/com/udemy/webservices/config/TestConfig.java
+++ src/main/java/com/udemy/webservices/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.udemy.webservices.entities.Order;
 import com.udemy.webservices.entities.User;
+import com.udemy.webservices.entities.enums.OrderStatus;
 import com.udemy.webservices.repositories.OrderRepository;
 import com.udemy.webservices.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "999998888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "999977777", "123456");
 		
-		Order order1 = new Order(null, Instant.parse("2021-02-20T19:52:37Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2021-03-20T21:33:42Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2021-04-20T15:07:02Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2021-02-20T19:52:37Z"), user1, OrderStatus.WAITING_PAYMENT);
+		Order order2 = new Order(null, Instant.parse("2021-03-20T21:33:42Z"), user2, OrderStatus.WAITING_PAYMENT);
+		Order order3 = new Order(null, Instant.parse("2021-04-20T15:07:02Z"), user1, OrderStatus.WAITING_PAYMENT);
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
